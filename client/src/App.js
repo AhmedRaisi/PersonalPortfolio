4edcd04486dda79bf2b6ce7ef101b99e3f41bc22
alrai014 <alrai014@umn.edu>
2023-09-24 15:03:18 -0500

    added nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function Home() {
+  return <div>Home content goes here.</div>;
+}
+
+function About() {
+  return <div>About content goes here.</div>;
+}
+
+// ... other component definitions ...
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <h1>Welcome to my Portfolio</h1>
-        <h1>It is a work in progress</h1>
-      </header>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* Add routes for Projects, Blog, and Contact */}
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
