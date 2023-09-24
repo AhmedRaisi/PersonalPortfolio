7a2b0ba781a11e946355a8d8144a844d279888d8
alrai014 <alrai014@umn.edu>
2023-09-24 14:04:23 -0500

    started react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my Portfolio</h1>
+        <h1>It is a work in progress</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
